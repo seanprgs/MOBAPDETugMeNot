7848d325832caf9bcca6613252a65aaadecf1530
Sean Paragas <sean_paragas@dlsu.edu.ph>
2017-11-14 18:09:19 +0800

    Finished menu screen

--- app/src/main/java/edu/dlsu/mobapde/mobapdetugmenot/MainActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/mobapdetugmenot/MainActivity.java
@@ -1,13 +1,28 @@
 package edu.dlsu.mobapde.mobapdetugmenot;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//main menu
-public class MainActivity extends AppCompatActivity {
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
+//main menu
+public class MainActivity extends Activity
+{
+    ImageButton ibOne, ibTwo, ibTutorial, ibSettings;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        ibOne = findViewById(R.id.ib_one);
+        ibTwo = findViewById(R.id.ib_two);
+        ibTutorial = findViewById(R.id.ib_tutorial);
+        ibSettings = findViewById(R.id.ib_settings);
     }
 }
--- app/src/main/java/edu/dlsu/mobapde/mobapdetugmenot/TutorialActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/mobapdetugmenot/TutorialActivity.java
@@ -1,13 +1,27 @@
 package edu.dlsu.mobapde.mobapdetugmenot;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
 //tutorial screen
-public class TutorialActivity extends AppCompatActivity {
+public class TutorialActivity extends Activity
+{
+
+    ImageButton ibBack;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_tutorial);
+
+        ibBack = findViewById(R.id.ib_back);
     }
 }
