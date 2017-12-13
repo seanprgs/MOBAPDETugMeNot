3fc96f1388d0ede31c1bcf45b9e28de3f4f8739e
Sean Paragas <sean_paragas@dlsu.edu.ph>
2017-12-13 18:58:22 +0800

    stuff

--- app/src/main/java/edu/dlsu/mobapde/mobapdetugmenot/GameActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/mobapdetugmenot/GameActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -14,8 +16,11 @@ import android.widget.LinearLayout;
 
 //game screen
 public class GameActivity extends Activity {
+
+    GestureDetector gestureDetector;
     LinearLayout gameView;
     ImageButton ibPause;
+    public static final int GAME_MODE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,8 @@ public class GameActivity extends Activity {
         gameView = findViewById(R.id.game);
         gameView.addView(gameV);
 
+        gestureDetector = new GestureDetector(getBaseContext(), onGestureListener);
+
         ibPause = findViewById(R.id.ib_pause);
 
         ibPause.setOnClickListener(new View.OnClickListener() {
@@ -39,5 +46,30 @@ public class GameActivity extends Activity {
                 startActivity(i);
             }
         });
+    }
+
+    GestureDetector.SimpleOnGestureListener onGestureListener
+            = new GestureDetector.SimpleOnGestureListener(){
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            // method triggered when swipe event is detected
+
+            // check if game is 1P or 2P
+
+            // locate touch event
+
+            // check player action prompt
+                // was action done?
+
+
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    };
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // method triggered when touch event is detected
+
+        return gestureDetector.onTouchEvent(event);
     }
 }
